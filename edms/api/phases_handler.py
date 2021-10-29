bea315c7a3a17531030c1817ac601d676f9d4ade
Stanislav Khramtsov <sauron4er@gmail.com>
2021-10-29 16:09:34 +0300

    edms hot-fix

--- edms/api/phases_handler.py
+++ edms/api/phases_handler.py
@@ -178,7 +178,10 @@ def handle_phase_marks(doc_request, phase_info):
 
     else:
         # Визначаємо усіх отримувачів для кожної позначки:
-        recipients = get_phase_recipient_list(phase_info['id'], doc_request['doc_type_version'])
+        if 'doc_type_version' in doc_request:
+            recipients = get_phase_recipient_list(phase_info['id'], doc_request['doc_type_version'])
+        else:
+            recipients = get_phase_recipient_list(phase_info['id'])
         for recipient in recipients:
             recipient = vacation_check(recipient)
             new_mail('new', [{'id': recipient}], doc_request)
