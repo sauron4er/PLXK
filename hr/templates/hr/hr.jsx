57a90f203d4fcf246ebd66710bd05f752bd9d98e
Stanislav Khramtsov <sauron4er@gmail.com>
2022-11-04 16:28:22 +0200

    hr hot-fix
    Виправив hr bundle

--- hr/templates/hr/hr.jsx
+++ hr/templates/hr/hr.jsx
@@ -1,2 +1,27 @@
-import { DevSupport } from "@react-buddy/ide-toolbox";
-import { ComponentPreviews, useInitial } from "../../../dev";
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import 'react-responsive-modal/styles.css';
+import Instructions from "./instructions/instructions";
+// import OrgStructure from "hr/templates/hr/org_structure_old/org_structure";
+import OrgStructure from "./org_structure/org_structure";
+
+class Hr extends React.Component {
+  state = {
+    main_div: document.getElementById("bundle").parentNode.id,
+  }
+  
+  render() {
+    return (
+      <Choose>
+        <When condition={this.state.main_div === 'instructions'}>
+          <Instructions/>
+        </When>
+        <When condition={this.state.main_div === 'org_structure'}>
+          <OrgStructure/>
+        </When>
+      </Choose>
+    );
+  }
+}
+
+ReactDOM.render(<Hr />, document.getElementById('bundle'));
