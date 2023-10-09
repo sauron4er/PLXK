19ed3c4be3fa941f13d6203c1c0d843c826260fc
Stanislav Khramtsov <sauron4er@gmail.com>
2023-10-09 14:18:14 +0300

    add scripts

---
+++ start_auto_operations.bat
@@ -0,0 +1,4 @@
+cd C:\venv_p39\Scripts
+call activate.bat
+cd C:\PLXK
+python manage.py serverless_cron_run
---
+++ start_site.bat
@@ -0,0 +1,4 @@
+cd C:\venv_p39\Scripts
+call activate.bat
+cd C:\PLXK
+python manage.py runserver 0.0.0.0:80
