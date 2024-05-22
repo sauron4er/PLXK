817b4f883b7f9c983774b2e2a777020bb3a5e73f
Stanislav Khramtsov <sauron4er@gmail.com>
2024-05-22 15:36:59 +0300

    print to pdf work

--- edms/templates/edms/my_docs/doc_info/pdf/doc_to_pdf.js
+++ edms/templates/edms/my_docs/doc_info/pdf/doc_to_pdf.js
@@ -4,22 +4,36 @@ import {faFilePdf} from '@fortawesome/free-solid-svg-icons';
 import ReactToPrint from 'react-to-print';
 import PDFRow from 'edms/templates/edms/my_docs/doc_info/pdf/pdf_row';
 import PDFCell from "edms/templates/edms/my_docs/doc_info/pdf/pdf_cell";
+import { getBooleanByQueue, getDayByQueue, getTextByQueue } from "templates/components/my_extras";
 
 function DocToPDF(props) {
   let componentRef = null;
+
+  console.log(props.info);
 
   const hideStyle = {
     display: 'none'
   };
 
-  function getModuleValue(module) {
+  function getModuleValue(module, index) {
+    switch (module.module) {
+      case 'text':
+        return getTextByQueue(props.info.text_list, index);
+      case 'day':
+        return getDayByQueue(props.info.days, index);
+      case 'dep_seat':
+        return props.info.dep_seat.dep_name + ' – ' + props.info.dep_seat.seat_name;
+      case 'boolean':
+        const bln = getBooleanByQueue(props.info.booleans, index)
+        return bln.checked ? 'Так' : 'Ні'
+
+    }
+
     return 'value'
     // boolean - вертати слово Так чи Ні
     // dep_seat - вертати відділ і посаду
     
   }
-
-  console.log(props.info.type_modules);
 
   return (
     <>
@@ -39,7 +53,7 @@ function DocToPDF(props) {
           {/*<PDFRow>*/}
           <div className={`row mt-3 mx-1`}>
             <For each='module' index='index' of={props.info.type_modules}>
-              <PDFCell key={index} label={module.field_name} value={getModuleValue(module)} columns={module.columns}/>
+              <PDFCell key={index} label={module.field_name} value={getModuleValue(module, index)} columns={module.columns}/>
             </For>
           </div>
           {/*</PDFRow>*/}
